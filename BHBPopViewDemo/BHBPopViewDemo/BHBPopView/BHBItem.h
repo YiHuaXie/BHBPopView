367998cc774810caf7aa0e21546cb41d85cc0cd3
NeroXie <xyh30902@163.com>
2019-10-30 14:22:38 +0800

    code update

--- BHBPopViewDemo/BHBPopViewDemo/BHBPopView/BHBItem.h
+++ BHBPopViewDemo/BHBPopViewDemo/BHBPopView/BHBItem.h
@@ -6,12 +6,12 @@
 //  Copyright (c) 2015年 毕洪博. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface BHBItem: NSObject
 
-@property (nonatomic,copy) NSString *title;
-@property (nonatomic,copy) NSString *icon;
+@property (nonatomic, copy) NSString *title;
+@property (nonatomic, copy) NSString *icon;
 
 - (instancetype)initWithTitle:(NSString *)title Icon:(NSString *)icon;
 
--- BHBPopViewDemo/BHBPopViewDemo/BHBPopView/UIButton+BHBSetImage.m
+++ BHBPopViewDemo/BHBPopViewDemo/BHBPopView/UIButton+BHBSetImage.m
@@ -11,11 +11,13 @@
 @implementation UIButton (BHBSetImage)
 
 - (void)bhb_setImage:(NSString *)imagePath{
-    NSString * filePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:imagePath];
+    NSString *filePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:imagePath];
     if([[UIDevice currentDevice].systemVersion doubleValue] < 8.0){
         filePath = [filePath stringByAppendingString:@"@2x"];
     }
-    UIImage * image = [UIImage imageWithContentsOfFile:filePath];
+    UIImage *image = [UIImage imageWithContentsOfFile:filePath];
+    if (!image) { image = [UIImage imageNamed:imagePath]; }
+    
     [self setImage:image forState:UIControlStateNormal];
 }
 
@@ -25,6 +27,7 @@
         filePath = [filePath stringByAppendingString:@"@2x"];
     }
     UIImage * image = [UIImage imageWithContentsOfFile:filePath];
+    if (!image) { image = [UIImage imageNamed:imagePath]; }
     image = [image resizableImageWithCapInsets:UIEdgeInsetsMake(image.size.height / 2 - 1, image.size.width / 2 - 1, image.size.height / 2 - 1, image.size.width / 2 - 1) resizingMode:UIImageResizingModeStretch];
     [self setBackgroundImage:image forState:UIControlStateNormal];
 }
